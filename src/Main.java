438834af5de0508b501c8dfe071a2b1443d44ad8
feller <cs-feller@yandex.ru>
2025-04-16 15:57:35 +0300

    Dz - HashMap

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,35 @@
-import taxes.TaxSystem;
-import taxes.TaxSystemDebit;
-import taxes.TaxSystemDebitCredit;
-
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args){
-        TaxSystem taxSystemDebit = new TaxSystemDebit();
-        TaxSystem taxSystemDebitCredit = new TaxSystemDebitCredit();
 
-        Company[] companies = {
-                new Company("Elen",taxSystemDebit),
-                new Company("Artem",taxSystemDebitCredit),
-                new Company("Kot",taxSystemDebit),
-                new Company("Kohka",taxSystemDebit),
-        };
-
-        for(Company company : companies){
-            company.shiftMoney(-10);
-            company.payTaxes();
-        }
     }
 }
 
 
+class Address{
+    protected String country;
+    protected String city;
+
+    public Address(String country, String city){
+        this.country = country;
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + country + "," + city + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj){
+            return true;
+        }
+        Address address = (Address) obj;
+        return this.country.equals(address.country) && this.city.equals(address.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country);
+    }
+}
